2c219592368957c2e2e57cbc640e787e8c200f34
sowmya <vasavisowmyavinnakota@gmail.com>
2025-02-22 22:57:51 +0530

    client server implementation

---
+++ client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/sowmya/grpc-go/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewGreetServiceClient(conn)
+
+	names := &pb.NamesList{
+		Names: []string{"Akhil", "Alice", "Bob"},
+	}
+
+	// callSayHello(client)
+	// callSayHelloServerStream(client, names)
+	// callSayHelloClientStream(client, names)
+	callSayHelloBidirectionalStream(client, names)
+}
